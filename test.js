9886d877f1a3c372195603e95aeaff2b0712edb9
Richard Lee <rich.leeroy@gmail.com>
2019-01-27 11:45:36 -0500

    add image

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
-console.log('Hello World');
+// console.log('Hello World');
 
-console.log(2+2);
+// console.log(2+2);
+
+var filesYo = require('fs');
+var https = require('https');
+
+filesYo.writeFile(__dirname + "/index.html","<h1>HTML rocks</h1>");
+
+var picURL = "https://picsum.photos/200";
+var picFile = filesYo.createWriteStream(__dirname + "/node-pic.jpg");
+var request = https.get(picURL, function(response){
+   response.pipe(picFile);
+});
