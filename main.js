4c310879ae14c2a9bb1ed2bec62a1958b57bdb15
richardl33 <rich.leeroy@gmail.com>
2019-01-28 21:12:14 -0500

    update parseFloat

--- main.js
+++ main.js
@@ -35,12 +35,17 @@ function readLine() {
 *   secondString - A string consisting of one or more space-separated words.
 **/
 
-var secondInteger = "5",
-secondDecimal = "8.0",
-secondString = "is the best place to learn and practice coding!";
+var secondInteger = "12",
+    secondDecimal = "4.32",
+    secondString = "is the best place to learn and practice coding!",
+    secVal;
 
 var parseNumbers = function(secArg){
-   var secVal = parseInt(secArg);
+   if ((secArg - Math.floor(secArg)) !== 0) {
+      secVal = parseFloat(secArg);
+   } else {
+      secVal = parseInt(secArg);
+   }
    return secVal;
 }
 
@@ -63,9 +68,12 @@ function performOperation(secondInteger, secondDecimal, secondString) {
     // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
     var secondDecimal = firstDecimal + parseNumbers(secondDecimal);
     var secondDecimalTotal = secondDecimal;
+
+    console.log(secondDecimalTotal);
     
     // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
     var stringConcat = firstString + secondString;
+
     console.log(stringConcat);
 }
 
