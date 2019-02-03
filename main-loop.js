945a9125b9b3bcddc5352c67c9c6e51fe61d866a
Richard Lee <rich.leeroy@gmail.com>
2019-02-02 22:58:58 -0500

    print letters

--- main-loop.js
+++ main-loop.js
@@ -2,10 +2,7 @@
 
 var s = ['j','a','v','a','s','c','r','i','p','t','l','o','o','p','s'];
 var vowels = ['a', 'e', 'i', 'o', 'u'];
-var sLoop;
-var vLoop;
-var vowels;
-var vowelsNull;
+
 
 function vowelsAndConsonants() {
    for (var b = 0; b < vowels.length; b++) {
@@ -15,19 +12,18 @@ function vowelsAndConsonants() {
          if (aLoop == bLoop) {
             console.log(s[a]);
          }
-         // if (aLoop !== bLoop && aLoop.indexOf('o') == bLoop.indexOf('o')) {
-         //    console.log(s[a]);
-         // }
       }
    }
 
-   // for (var i = 0; i < vowels.length; i++) {
-   //    vLoop = vowels[i];
-   //    for (var i = 0; i < s.length; i++) {
-   //       sLoop = s[i];
-
-   //    }
-   // }
+   for (var i = 0; i < vowels.length; i++) {
+      var vLoop = vowels[i];
+      for (var i = 0; i < s.length; i++) {
+         var sLoop = s[i];
+         if (vLoop !== sLoop && vLoop.indexOf('o') == sLoop.indexOf('o')) {
+            console.log(s[i]);
+         }
+      }
+   }
 }
 
 vowelsAndConsonants();
